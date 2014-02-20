7bd89723498a108c48cf868c3a1f3b5ed85301ff
Florent Lebreton <florentlebreton@free.fr>
2014-02-20 18:53:06 +0100

    Fix settings for disqus

--- settings.py
+++ settings.py
@@ -33,7 +33,7 @@ DEFAULT_PAGINATION = 5
 
 GOOGLE_ANALYTICS = 'UA-18281356-9'
 
-DISQUS_SITENAME = 'fle.github.io'
+DISQUS_SITENAME = 'flegithubio'
 
 PELICAN_SIMPLEGREY_STICKY_SIDEBAR = True
 PELICAN_SIMPLEGREY_ABOUT = "Software engineer and trainer at Makina Corpus Nantes. Python, Django and GIT addict. Dance, sail, take photos."
